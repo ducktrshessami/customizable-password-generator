fd607dbcb2e5d27d2213ed3c0011b71c1dd7989b
ducktrshessami <jwee761@gmail.com>
2020-10-11 19:19:42 -0700

    finished pass generator script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,9 +7,9 @@ var charsetSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; // I really don't l
 var lenMin = 8, lenMax = 128, lenDefault = 8;
 var lastUsed;
 
-// Random integer in range; min/max are inclusive
+// Random integer in range; min is inclusive, max is exclusive
 function randIntInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // Write password to the #password input
@@ -23,16 +23,24 @@ function writePassword() {
 
 // Prompt user for password criteria
 function getPasswordCriteria() {
-  var criteria = {};
-  if (lastUsed && prompt("Use same settings?")) { // For styling purposes, should I be doing Boolean(lastUsed) or is this fine?
+  var criteria = {charset: ""};
+  if (lastUsed && confirm("Use same settings?")) { // For styling purposes, should I be doing Boolean(lastUsed) or is this fine?
     criteria = lastUsed;
   }
   else {
-    criteria.passLength = Math.min(Math.max(parseInt(prompt("Length of password?")) || lenDefault, lenMin), lenMax);
-    criteria.lowercase = confirm("Include lowercase characters?");
-    criteria.uppercase = confirm("Include uppercase characters?");
-    criteria.numeric = confirm("Include numbers?");
-    criteria.special = confirm("Include special characters?");
+    criteria.passLength = Math.min(Math.max(parseInt(prompt("Length of password? (Minimum: " + lenMin + ", Maximum: " + lenMax + ")")) || lenDefault, lenMin), lenMax);
+    if (confirm("Include lowercase characters?")) { // Is minimizing memory usage really worth a 163 character long line?
+      criteria.charset += charsetLowercase;
+    }
+    if (confirm("Include uppercase characters?")) {
+      criteria.charset += charsetUppercase;
+    }
+    if (confirm("Include numeric characters?")) {
+      criteria.charset += charsetNumbers;
+    }
+    if (confirm("Include special characters?")) {
+      criteria.charset += charsetSpecial;
+    }
     lastUsed = criteria;
   }
   return criteria;
@@ -40,9 +48,12 @@ function getPasswordCriteria() {
 
 // Generate password based on criteria
 function generatePassword() {
-  var criteria = getPasswordCriteria();
-
+  var criteria = getPasswordCriteria(), password = "";
+  for (let i = 0; i < criteria.passLength; i++) {
+    password += criteria.charset[randIntInRange(0, criteria.charset.length)];
+  }
+  return password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // I feel like Brian told us we'd need to look this up, but here we are
